0a816ac8f71c4b6adad28a6e2b85cdca04a0ee34
Ionut Mocanu <Ionut.Mocanu@sparkware.ro>
2019-04-25 12:32:04 +0300

    commit asteroids

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
 
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Nasa logo" src="./assets/logo.png">
+    <HomePage msg="Welcome to Nasa"/>
     
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/homepage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
